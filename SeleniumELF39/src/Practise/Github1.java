d71d9a22150cffebc09e80fd45b3b7caa7794fcd
91789 <91789@SyedAdnan-07>
2022-11-24 16:58:29 +0530

    new class github1.java from practise

---
+++ SeleniumELF39/src/Practise/Github1.java
@@ -0,0 +1,11 @@
+package Practise;
+
+public class Github1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("pratiksha new class");
+
+	}
+
+}
