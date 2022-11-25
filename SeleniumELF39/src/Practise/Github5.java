754ce74f9570b827a3e25bdcdf733a540d53cd81
91789 <91789@SyedAdnan-07>
2022-11-25 10:34:29 +0530

    new class github5

---
+++ SeleniumELF39/src/Practise/Github5.java
@@ -0,0 +1,11 @@
+package Practise;
+
+public class Github5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("edfrgthyujiuytrftyuio");
+
+	}
+
+}
